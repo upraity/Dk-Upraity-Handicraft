fedea7b7723a64a67442c53e2053259535b3c4ef
Dj <134566303+upraity@users.noreply.github.com>
2024-08-31 21:22:16 +0530

    Add files via upload

---
+++ ind.php
@@ -0,0 +1,31 @@
+<?php
+    // print_r($_SERVER);
+    // $request = $_SERVER['REQUEST_URI'];
+    // echo $request;
+    // $router = $request;
+    
+    $router = $_SERVER['REQUEST_URI'];
+
+    // echo $router;
+    if($router == '/index'){
+        include('index.php');
+    }
+    // elseif($router == '/logout' || $router == '/logout?i=1'){
+    //     require('logout.php');
+    // } 
+    elseif($router == '/contact' ||$router == '/contact?i=1' ||$router == '/contactus'){
+        include('contactus.php');
+    }
+    // elseif($router == '/forgotpassword' || $router == '/forgotpassword?i=1'){
+    //     require('forgotpassword.php');
+    // } 
+    elseif($router == '/search' ||$router == '/search?i=1'){
+        include('search.php');
+    }
+    elseif($router == '/profile' ||$router == '/profile?i=1'){
+        include('profile.php');
+    }
+    else{
+        header("location:https://dkupraityhandicraft.000.pe/");
+    }
+?>
