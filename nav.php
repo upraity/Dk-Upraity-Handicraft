611e9224d52220cb5a01548c6fa844e113904939
Dj <134566303+upraity@users.noreply.github.com>
2024-09-05 21:18:13 +0530

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,50 @@
+<style>
+    .footer {
+        background-color: #333;
+        color: #fff;
+        text-align: center;
+        height: 8%;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        padding:5px;
+        z-index:1;
+    }
+    .footer a {
+        color: #fff;
+        text-decoration: none;
+    }
+    .footer a:hover {
+        text-decoration: underline;
+    }
+</style>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="https://dkupraityhandicraft.000.pe">Dk Upraity Handicrafts</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="https://dkupraityhandicraft.000.pe/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <!--<li class="nav-item">
+        <a class="nav-link" href="https://dkupraityhandicraft.000.pe/items">Items</a>
+      </li>-->
+      <li class="nav-item">
+        <a class="nav-link" href="https://dkupraityhandicraft.000.pe/contactus">Contact Us</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0" action="https://dkupraityhandicraft.000.pe/search.php" method="GET">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="footer1" >
+    <footer class="footer">
+        <p><b>&copy; 2024</b> Dk Upraity Handicraft. All Rights Reserved.<br> Designed by <b><a href="https://dj.000.pe/portfolio">Dj Upraity</a></b></p>
+    </footer>
+</div>
